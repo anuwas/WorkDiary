c6fa3676d0f3043dddbb23aff5f63877515ac4f4
ANUWAS <info.anuwas@gmail.com>
2021-10-27 01:39:36 +0530

    highp

--- src/main/java/com/org/pack/wd/util/ConstantProperties.java
+++ src/main/java/com/org/pack/wd/util/ConstantProperties.java
@@ -1,12 +1,15 @@
 package com.org.pack.wd.util;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ConstantProperties {
 
-	public static final Map<Integer, String> TASK_PRIORITY_MAP = Map.of(
-	        1, "High",
-	        2, "Medium",
-	        3, "Low"
-	    );
+	public static  Map<Integer, String> TASK_PRIORITY_MAP;
+	static {
+		TASK_PRIORITY_MAP = new HashMap<>();
+		TASK_PRIORITY_MAP.put(1, "High");
+		TASK_PRIORITY_MAP.put(2, "Medium");
+		TASK_PRIORITY_MAP.put(3, "Low");
+    }
 }
